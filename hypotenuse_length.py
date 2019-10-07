868debbcfcb9718370932852293a95e0174f507d
G Sri Harsha <54752266+GSri30@users.noreply.github.com>
2019-10-07 12:25:40 +0530

    Add files via upload

---
+++ hypotenuse_length.py
@@ -0,0 +1,10 @@
+# code to find the length of hypotenuse
+
+import math
+
+print("Enter the value of a: ")
+a=int(input())
+print("Enter the value of b: ")
+b=int(input())
+c=math.sqrt(a**2+b**2)
+print("The length of the hypotenuse is", c)
