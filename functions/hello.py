4454e7d681b681930011a157f2ebe8ca1e44fa28
G Sri Harsha <54752266+GSri30@users.noreply.github.com>
2019-10-24 03:05:34 +0530

    Add files via upload

---
+++ functions/hello.py
@@ -0,0 +1,9 @@
+#Implement a function hello(name) that returns a string with ”Hello ” as prefix to name. (name is a string input.)
+
+def hello(name):
+## Your code - begin
+    
+    return "Hello "+name
+
+## Your code - end
+  
