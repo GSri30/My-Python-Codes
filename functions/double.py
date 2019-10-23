f0dfe978e0c9dd4ecef7151daeb8612f60fe87d9
G Sri Harsha <54752266+GSri30@users.noreply.github.com>
2019-10-24 03:06:36 +0530

    Add files via upload

---
+++ functions/double.py
@@ -0,0 +1,11 @@
+#Implement a function double(l) that takes an input list l and returns a list doubling every element of l . Use list comprehension to achieve this.
+
+def double(l):
+## Your code - begin
+
+    #directly return a list which has elements double of the given list by iterating through each element and doubling it 
+    return [2*x for x in l]
+
+## Your code - end
+  
+
